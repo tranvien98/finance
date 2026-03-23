3f9ebb5a982c23046f0a4a645ce62ab4c7ddc9d8
tranvien98 <tranvanvien98bg@gmail.com>
2026-03-24 00:22:43 +0700

    fix(shadcn): bypass legendprops constraint errors

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -259,11 +259,7 @@ function ChartLegendContent({
   payload,
   verticalAlign = "bottom",
   nameKey,
-}: React.ComponentProps<"div"> &
-  Pick<RechartsPrimitive.LegendProps, "payload" | "verticalAlign"> & {
-    hideIcon?: boolean
-    nameKey?: string
-  }) {
+}: any) {
   const { config } = useChart()
 
   if (!payload?.length) {
